5fba5fc6427b213b4f180ef4a9a733da84b5e5cb
Lady Trell <antrellkent@gmail.com>
2022-04-02 21:01:19 -0400

    used inquirer module to collect name from commandline

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+  
+/*
 // Declare reference to the filesystem
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-
-// Assignment destructuring 
-const [name, github] = profileDataArgs;
+const pageHTML = generatePage(name, github);
 
 
   
@@ -14,8 +24,13 @@ fs.writeFile('index.html', generatePage(name, github), err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
+*/
 
 /*
+
+// Assignment destructuring 
+const [name, github] = profileDataArgs;
+
 const printProfileData = profileDataArr => {
   // This...
   for (let i = 0; i < profileDataArr.length; i += 1) {
